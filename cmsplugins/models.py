13c00e5b6017ce4e29da5dd66fca242840c0148d
Max <noelpmax@gmail.com>
2024-10-18 17:37:39 +0200

    POT-002 create ContainerModel in models.py for Container

--- cmsplugins/models.py
+++ cmsplugins/models.py
@@ -1,3 +1,20 @@
+from cms.plugin_model import CMSPlugin
+from colorfield.fields import ColorField
 from django.db import models
+from django.utils.translation import gettext as _
+from django_text.fields import HTMLField
+
+from .utils import COLOR_PALETTE
+
+
+class ContainerModel(CMSPlugin):
+    title = models.CharField(_("Titel eingeben"), max_length=50)
+    background_color = ColorField(
+        _("Farbe auswählen"), choices=COLOR_PALETTE, default="#000000"
+    )
+
+    def __str__(self):
+        return self.title
+
 
 # Create your models here.
