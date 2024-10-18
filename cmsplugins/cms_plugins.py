31d97556b0fc9e4005f85e913b0411cfff319fe1
Max <noelpmax@gmail.com>
2024-10-18 20:30:51 +0200

    POT-002 create Plugin for ContainerModel

---
+++ cmsplugins/cms_plugins.py
@@ -0,0 +1,18 @@
+from cms.models.pluginmodel import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+from django.utils.translation import gettext_lazy as _
+
+from .models import ContainerModel
+
+
+@plugin_pool.register_plugin
+class ContainerModelPlugin(CMSPluginBase):
+    model = ContainerModel
+    name = _("POT-002 CMSPlugin Container")
+    render_template = "container/container.html"
+    cache = False
+    allow_children = True
+
+    def render(self, context, instance, placeholder):
+        context = super().render(context, instance, placeholder)
+        return context
